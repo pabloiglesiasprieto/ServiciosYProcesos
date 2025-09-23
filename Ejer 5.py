b645d0ef57aced6d660bd8ddff7b4899a29ef781
pablo.iglesias <pablo.iglesias@iesnervion.es>
2025-09-23 19:47:45 +0200

    Ejercicios hasta el 4

--- Ejer 5.py
+++ Ejer 5.py
@@ -1,2 +1,10 @@
 # Escribir una aplicación para aprender a contar, que pedirá un número n y mostrará todos los números del 1 al n.
 
+# Preguntamos el número.
+valor1 = int(input("Introduce un número: "))
+
+# Creamos un bucle desde 1 hasta el valor dado.
+for valor in range(1,valor1 + 1):
+    
+    # Imprimimos el valor.
+    print(valor)
