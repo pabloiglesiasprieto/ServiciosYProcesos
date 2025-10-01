0cd971f1535bf39fc0ed320e7987336e63e3ba25
pabloiglesiasprieto <pablo.iglesias@iesnervion.es>
2025-10-01 10:01:53 +0200

    colecciones terminadas

--- Tema1/Ejer 10.py
+++ Tema1/Ejer 10.py
@@ -1,1 +1,21 @@
-#Crea un diccionario donde las claves sean el conjunto 1 de la siguiente tabla y los valores, el conjunto 2:
+# Crea un diccionario donde las claves sean el conjunto 1 de la siguiente tabla y los valores, el conjunto 2:
+# El programa debe pedir una frase al usuario y debe mostrar la frase encriptada. 
+# Para ello, cada vez que encuentre en la frase una letra del conjunto 1 la sustituirá por su correspondiente en el conjunto 2.
+
+# Creamos el diccionario con la correspondencia de los 2 conjuntos.
+correspondencia_str = {'e': 'p', 'i': 'v', 'k': 'i', 'm': 'u', 'p': 'm', 'q': 't', 'r': 'e', 's': 'r', 't': 'k', 'u': 'q', 'v': 's'}
+
+# Le preguntamos la frase al usuario.
+frase = str(input("Introduce la frase a encriptar: "))
+
+# Creamos una variable string vacia.
+fraseCrypted = ""
+
+# Creamos un bucle que recorra la frase.
+for letra in frase:
+    
+    # Vamos comprobando la correspondencia.
+    fraseCrypted += correspondencia_str.get(letra, letra) 
+    
+# Imprimimos la frase encriptada.
+print(fraseCrypted)
--- Tema1/Ejer 9.py
+++ Tema1/Ejer 9.py
@@ -1,1 +1,28 @@
-# Crea un diccionario donde las claves son las letras del abecedario y los valores, la puntuación para cada letra, como en el Scrabble. El programa le pedirá una palabra al usuario y se mostrará por pantalla la puntuación que tiene la palabra en total.
+# Crea un diccionario donde las claves son las letras del abecedario y los valores, la puntuación para cada letra, como en el Scrabble.
+# El programa le pedirá una palabra al usuario y se mostrará por pantalla la puntuación que tiene la palabra en total.
+
+# Creamos un diccionario con todas las letras a 0.
+diccionario = {
+    "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0,
+    "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0,
+    "ñ": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0,
+    "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0
+}
+
+# Preguntamos la frase al usuario.
+frase = str(input("Introduce una frase: "))
+
+# Quitamos los espacios de la frase.
+lista = frase.split(" ")
+
+# Hacemos uso de un bucle anidado para que recorra la lista y a su misma vez recorra las letras de las palabras de la lista.
+for palabras in lista:
+    
+    # Recorremos las letras de la palabra.
+    for letras in palabras:
+        
+        # Incrementamos en 1 el valor de la key.
+        diccionario[letras.lower()] += 1
+
+# Imprimimos el diccionario.
+print(diccionario)
