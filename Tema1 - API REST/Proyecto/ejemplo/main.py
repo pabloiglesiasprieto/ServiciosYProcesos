a7d684f46ce79c7b777a34adcffd109cd704ac66
pabloiglesiasprieto <pablo.iglesias@iesnervion.es>
2025-10-22 17:42:26 +0200

    si

--- Tema1 - API REST/Proyecto/ejemplo/main.py
+++ Tema1 - API REST/Proyecto/ejemplo/main.py
@@ -13,156 +13,43 @@ class Director(BaseModel):
 
 
 listaDirectores = [
-    {
-        "id": 1,
-        "dni": "12345678A",
-        "nombre": "Steven",
-        "apellidos": "Spielberg",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 2,
-        "dni": "23456789B",
-        "nombre": "Martin",
-        "apellidos": "Scorsese",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 3,
-        "dni": "34567890C",
-        "nombre": "Christopher",
-        "apellidos": "Nolan",
-        "nacionalidad": "Británico",
-    },
-    {
-        "id": 4,
-        "dni": "45678901D",
-        "nombre": "Quentin",
-        "apellidos": "Tarantino",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 5,
-        "dni": "56789012E",
-        "nombre": "Pedro",
-        "apellidos": "Almodóvar",
-        "nacionalidad": "Español",
-    },
-    {
-        "id": 6,
-        "dni": "67890123F",
-        "nombre": "Guillermo",
-        "apellidos": "del Toro",
-        "nacionalidad": "Mexicano",
-    },
-    {
-        "id": 7,
-        "dni": "78901234G",
-        "nombre": "Sofia",
-        "apellidos": "Coppola",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 8,
-        "dni": "89012345H",
-        "nombre": "Alejandro",
-        "apellidos": "González Iñárritu",
-        "nacionalidad": "Mexicano",
-    },
-    {
-        "id": 9,
-        "dni": "90123456I",
-        "nombre": "Greta",
-        "apellidos": "Gerwig",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 10,
-        "dni": "01234567J",
-        "nombre": "Denis",
-        "apellidos": "Villeneuve",
-        "nacionalidad": "Canadiense",
-    },
-    {
-        "id": 11,
-        "dni": "12345678K",
-        "nombre": "Bong",
-        "apellidos": "Joon-ho",
-        "nacionalidad": "Surcoreano",
-    },
-    {
-        "id": 12,
-        "dni": "23456789L",
-        "nombre": "Wes",
-        "apellidos": "Anderson",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 13,
-        "dni": "34567890M",
-        "nombre": "Kathryn",
-        "apellidos": "Bigelow",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 14,
-        "dni": "45678901N",
-        "nombre": "Alfonso",
-        "apellidos": "Cuarón",
-        "nacionalidad": "Mexicano",
-    },
-    {
-        "id": 15,
-        "dni": "56789012O",
-        "nombre": "Ava",
-        "apellidos": "DuVernay",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 16,
-        "dni": "67890123P",
-        "nombre": "David",
-        "apellidos": "Fincher",
-        "nacionalidad": "Estadounidense",
-    },
-    {
-        "id": 17,
-        "dni": "78901234Q",
-        "nombre": "Paolo",
-        "apellidos": "Sorrentino",
-        "nacionalidad": "Italiano",
-    },
-    {
-        "id": 18,
-        "dni": "89012345R",
-        "nombre": "Jane",
-        "apellidos": "Campion",
-        "nacionalidad": "Neozelandesa",
-    },
-    {
-        "id": 19,
-        "dni": "90123456S",
-        "nombre": "Park",
-        "apellidos": "Chan-wook",
-        "nacionalidad": "Surcoreano",
-    },
-    {
-        "id": 20,
-        "dni": "01234567T",
-        "nombre": "Ridley",
-        "apellidos": "Scott",
-        "nacionalidad": "Británico",
-    },
+    Director(id=1, dni="12345678A", nombre="Steven", apellidos="Spielberg", nacionalidad="Estadounidense"),
+    Director(id=2, dni="23456789B", nombre="Martin", apellidos="Scorsese", nacionalidad="Estadounidense"),
+    Director(id=3, dni="34567890C", nombre="Christopher", apellidos="Nolan", nacionalidad="Británico"),
+    Director(id=4, dni="45678901D", nombre="Quentin", apellidos="Tarantino", nacionalidad="Estadounidense"),
+    Director(id=5, dni="56789012E", nombre="Pedro", apellidos="Almodóvar", nacionalidad="Español"),
+    Director(id=6, dni="67890123F", nombre="Guillermo", apellidos="del Toro", nacionalidad="Mexicano"),
+    Director(id=7, dni="78901234G", nombre="Sofia", apellidos="Coppola", nacionalidad="Estadounidense"),
+    Director(id=8, dni="89012345H", nombre="Alejandro", apellidos="González Iñárritu", nacionalidad="Mexicano"),
+    Director(id=9, dni="90123456I", nombre="Greta", apellidos="Gerwig", nacionalidad="Estadounidense"),
+    Director(id=10, dni="01234567J", nombre="Denis", apellidos="Villeneuve", nacionalidad="Canadiense"),
+    Director(id=11, dni="12345678K", nombre="Bong", apellidos="Joon-ho", nacionalidad="Surcoreano"),
+    Director(id=12, dni="23456789L", nombre="Wes", apellidos="Anderson", nacionalidad="Estadounidense"),
+    Director(id=13, dni="34567890M", nombre="Kathryn", apellidos="Bigelow", nacionalidad="Estadounidense"),
+    Director(id=14, dni="45678901N", nombre="Alfonso", apellidos="Cuarón", nacionalidad="Mexicano"),
+    Director(id=15, dni="56789012O", nombre="Ava", apellidos="DuVernay", nacionalidad="Estadounidense"),
+    Director(id=16, dni="67890123P", nombre="David", apellidos="Fincher", nacionalidad="Estadounidense"),
+    Director(id=17, dni="78901234Q", nombre="Paolo", apellidos="Sorrentino", nacionalidad="Italiano"),
+    Director(id=18, dni="89012345R", nombre="Jane", apellidos="Campion", nacionalidad="Neozelandesa"),
+    Director(id=19, dni="90123456S", nombre="Park", apellidos="Chan-wook", nacionalidad="Surcoreano"),
+    Director(id=20, dni="01234567T", nombre="Ridley", apellidos="Scott", nacionalidad="Británico"),
 ]
 
 
+# region utils
 def findById(director_id):
-    lista = [director for director in listaDirectores if director["id"] == director_id]
+    lista = [director for director in listaDirectores if director.id == director_id]
     if lista:
         return lista[0]
     raise HTTPException(status_code=404, detail="Director no encontrado")
 
+def lastID():
+    if listaDirectores:
+        return max(listaDirectores, key=id).id+1
+    return 0
+#endregion utils
 
+# region gets
 @app.get("/directores")
 def get_directores():
     return listaDirectores
@@ -183,7 +70,7 @@ def get_directores_por_nacionalidad(nacionalidad: str):
     lista = [
         director
         for director in listaDirectores
-        if director["nacionalidad"].lower() == nacionalidad.lower()
+        if director.nacionalidad.lower() == nacionalidad.lower()
     ]
 
     if lista:
@@ -197,7 +84,7 @@ def get_directores_por_nombre(empieza_con: str):
     lista = [
         director
         for director in listaDirectores
-        if director["nombre"].lower().startswith(empieza_con.lower())
+        if director.nombre.lower().startswith(empieza_con.lower())
     ]
     if lista:
         return lista[0]
@@ -223,9 +110,36 @@ def get_directores_por_apellidos(apellidos: str):
     lista = [
         director
         for director in listaDirectores
-        if director["apellidos"].lower() == apellidos.lower()
+        if director.apellidos.lower() == apellidos.lower()
     ]
     if lista:
         return lista[0]
     else:
         raise HTTPException(status_code=404, detail="Director no encontrado")
+    
+#endregion gets
+#region posts
+@app.post("/directores", response_model=Director, status_code=201)
+def crear_director(director: Director):
+    director.id = lastID()
+    listaDirectores.append(director)
+    return director
+#endregion posts
+#region puts
+@app.put("/directores/{director_id}", response_model=Director)
+def actualizar_director(director_id: int, director_actualizado: Director):
+    for index, director in enumerate(listaDirectores):
+        if director.id == director_id:
+            director_actualizado.id = director_id
+            listaDirectores[index] = director_actualizado
+            return listaDirectores[index]
+    raise HTTPException(status_code=404, detail="Director no encontrado")
+#endregion puts
+#region deletes
+@app.delete("/directores/{director_id}")
+def eliminar_director(director_id: int):
+    for director in listaDirectores:
+        if director.id == director_id:
+            listaDirectores.remove(director)
+            return {}
+    raise HTTPException(status_code=404, detail="Director no encontrado")
